b448784d07c36c3dd18d5319ba17eef6207b3ce5
Fabxx <30447649+Fabxx@users.noreply.github.com>
2025-08-22 19:55:47 +0200

    [Core/All] Add MinGW Build for Wine testing

--- build_all.sh
+++ build_all.sh
@@ -4,18 +4,18 @@ cp -r "info" "build"
 
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" -DCORE="azahar" -DSYSTEM_NAME="Nintendo - Nintendo 3DS" .. && cmake --build . --parallel
+cmake -DCMAKE_BUILD_TYPE=Release -DCORE="azahar" -DSYSTEM_NAME="Nintendo - Nintendo 3DS" .. && cmake --build . --parallel
 
-cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" -DCORE="duckstation" -DSYSTEM_NAME="Sony - Playstation" .. && cmake --build . --parallel
+cmake -DCMAKE_BUILD_TYPE=Release -DCORE="duckstation" -DSYSTEM_NAME="Sony - Playstation" .. && cmake --build . --parallel
 
-cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" -DCORE="mgba" -DSYSTEM_NAME="Nintendo - Game Boy Advance" .. && cmake --build . --parallel
+cmake -DCMAKE_BUILD_TYPE=Release -DCORE="mgba" -DSYSTEM_NAME="Nintendo - Game Boy Advance" .. && cmake --build . --parallel
 
-cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" -DCORE="melonds" -DSYSTEM_NAME="Nintendo - Nintendo DS" .. && cmake --build . --parallel
+cmake -DCMAKE_BUILD_TYPE=Release -DCORE="melonds" -DSYSTEM_NAME="Nintendo - Nintendo DS" .. && cmake --build . --parallel
 
-cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" -DCORE="pcsx2" -DSYSTEM_NAME="Sony - Playstation 2" .. && cmake --build .  --parallel
+cmake -DCMAKE_BUILD_TYPE=Release -DCORE="pcsx2" -DSYSTEM_NAME="Sony - Playstation 2" .. && cmake --build .  --parallel
 
-cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" -DCORE="xemu" -DSYSTEM_NAME="Microsoft - Xbox" .. && cmake --build . --parallel
+cmake -DCMAKE_BUILD_TYPE=Release -DCORE="xemu" -DSYSTEM_NAME="Microsoft - Xbox" .. && cmake --build . --parallel
 
-cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" -DCORE="xenia" -DSYSTEM_NAME="Microsoft - Xbox 360" .. && cmake --build . --parallel
+cmake -DCMAKE_BUILD_TYPE=Release -DCORE="xenia" -DSYSTEM_NAME="Microsoft - Xbox 360" .. && cmake --build . --parallel
 
 find . -type f ! \( -name "*.dll" -o -name "*.so" \) -exec rm -f {} +
